c8a3e0f541eaf37b3d7bc578fe78971b6e65bf00
leducmanh0838 <leducmanh0838@gmail.com>
2025-09-02 23:05:04 +0700

    JOB-8 Khởi tạo môi trường frontend ReactJS

---
+++ frontend/src/configs/env.js
@@ -0,0 +1,5 @@
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+export const FACEBOOK_APP_ID= process.env.REACT_APP_FACEBOOK_APP_ID;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const DOT_CLIENT_ID = process.env.REACT_APP_DOT_CLIENT_ID;
+export const DOT_CLIENT_SECRET = process.env.REACT_APP_DOT_CLIENT_SECRET;
